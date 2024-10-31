a42d802b02e508e5ef721646465482e6ea5aa257
Neti-Sade <neti.s@zengo.com>
2024-10-31 18:26:54 +0200

    config fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,6 +8,8 @@ export const config = {
     process.env.REFRESH_TOKEN_SECRET || "your_refresh_token_secret",
   mongoUri: process.env.MONGODB_URI || "your_mongodb_uri",
   resendApiKey: process.env.RESEND_API_KEY || "your_resend api_key",
+  emailVerificationUrl:
+    process.env.EMAIL_VERIFICATION_URL || "http://localhost:3000/verify-email",
   allowedOrigins: (process.env.ALLOWED_ORIGINS?.split(",") || []) as string[],
 
   // Add other configuration variables as needed
