c7c4f509b201dd91d45792823498a3c3566594f8
Neti-Sade <neti.s@zengo.com>
2024-11-25 16:31:49 +0200

    clientDate fix

--- src/utils.ts
+++ src/utils.ts
@@ -1,24 +1,8 @@
-export const getClientDate = (dateString: string) => {
+export const getClientDateString = (dateString: string) => {
   const clientDate = new Date(dateString);
   if (isNaN(clientDate.getTime())) {
-    return null;
-  }
-  return clientDate;
-};
-
-export const getClientDateString = (dateString: string) => {
-  const clientDate = getClientDate(dateString);
-  if (!clientDate) {
     return null;
   }
 
   return clientDate.toISOString().split("T")[0];
 };
-
-export const compareDates = (date1: string, date2: string) => {
-  return getClientDateString(date1) === getClientDateString(date2);
-};
-
-
-
-
